4072ca3c40739091a835014f995eeaa77d6961b5
Nithin Thomas <nithinthomasninan1996@gmail.com>
2021-04-28 21:11:04 +0530

    - nithin

--- app/src/main/java/com/mobatia/naisapp/activity/home/HomeActivity.kt
+++ app/src/main/java/com/mobatia/naisapp/activity/home/HomeActivity.kt
@@ -2,6 +2,7 @@ package com.mobatia.naisapp.activity.home
 
 import android.content.Context
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.mobatia.naisapp.R
 
@@ -9,11 +10,10 @@ class HomeActivity :AppCompatActivity()
 {
     lateinit var mContext: Context
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         mContext = this
-
-
     }
 }
